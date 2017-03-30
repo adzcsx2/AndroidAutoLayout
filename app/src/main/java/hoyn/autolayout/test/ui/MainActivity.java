9ccf3535913c882c63b432d39cf5d406bd2cfb7d
Hoyn <hoyn.long@iim.ltd>
2017-03-30 17:31:24 +0800

    first add to my files

---
+++ app/src/main/java/hoyn/autolayout/test/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package hoyn.autolayout.test.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hoyn.autolayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
