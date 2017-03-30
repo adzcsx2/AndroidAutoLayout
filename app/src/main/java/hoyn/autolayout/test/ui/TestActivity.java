dafb7464189b3359c75d7202d4db50b60856a698
Hoyn <hoyn.long@iim.ltd>
2017-03-30 18:48:09 +0800

    del ui

--- app/src/main/java/hoyn/autolayout/test/ui/TestActivity.java
+++ app/src/main/java/hoyn/autolayout/test/ui/TestActivity.java
@@ -1,15 +1,14 @@
 package hoyn.autolayout.test.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import hoyn.autolayout.R;
-
-public class MainActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new TextView(this));
     }
 }
